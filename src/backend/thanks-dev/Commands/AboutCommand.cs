a03931386bb2cce98f6fe1b97d73f781e51c07bf
Christopher MANEU <git@maneu.org>
2022-12-06 11:59:49 +0100

    feat(dev): Scaffold Developer CLI
    Contributes to #19

---
+++ src/backend/thanks-dev/Commands/AboutCommand.cs
@@ -0,0 +1,18 @@
+﻿using Spectre.Console;
+using Spectre.Console.Cli;
+using System.Reflection;
+
+namespace thanks_dev.Commands;
+
+internal class AboutCommand : Command
+{
+    public override int Execute(CommandContext context)
+    {
+        AnsiConsole.Write(new FigletText("Thanks!").Color(Color.Cyan1));
+        AnsiConsole.MarkupLine("Thanks[cyan]giving[/][red]![/] developer CLI");
+
+        AnsiConsole.MarkupLineInterpolated($"Version: {Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}");
+
+        return 0;
+    }
+}
