c15f7282cce0fbd2a366e9b4b0ee7bb8142ba793
Christopher MANEU <git@maneu.org>
2022-12-06 11:59:49 +0100

    feat(dev): Scaffold Developer CLI
    Contributes to #19

---
+++ src/backend/thanks-dev/Program.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+using thanks_dev.Commands;
+
+var app = new CommandApp();
+app.Configure(config =>
+{
+    config.AddCommand<AboutCommand>("about")
+        .WithDescription("Displays CLI version information.");
+});
+
+return app.Run(args);
