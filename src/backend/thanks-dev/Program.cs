09bb05e1466d027ba6e88d4947ab959e7cd8bbb6
Christopher MANEU <git@maneu.org>
2022-12-06 12:23:43 +0100

    feat(dev): Add Health check command to API
    Related to #23 #19

--- src/backend/thanks-dev/Program.cs
+++ src/backend/thanks-dev/Program.cs
@@ -1,11 +1,28 @@
-﻿using Spectre.Console;
+﻿//#define ATTACH_DEBUGGER
+using Spectre.Console;
+using System.Diagnostics;
 using thanks_dev.Commands;
+
+#if ATTACH_DEBUGGER
+AnsiConsole.MarkupLine(":warning: [red]Attach debugger now :bug:[/]");
+Console.WriteLine("Attach debugger now");
+Console.ReadKey();
+#endif
+
 
 var app = new CommandApp();
 app.Configure(config =>
 {
     config.AddCommand<AboutCommand>("about")
         .WithDescription("Displays CLI version information.");
+
+
+    config.AddBranch("api", api =>
+    {
+        api.AddCommand<HealthCheckCommand>("health")
+            .WithDescription("Execute a health check on a deployed API")
+            .WithExample(new[] { "api","health", "http://localhost:8080" });
+    });
 });
 
 return app.Run(args);
