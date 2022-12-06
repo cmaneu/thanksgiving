4b72d2e2276bcabfdd6ef5e5887c205c964f2c1a
Christopher MANEU <git@maneu.org>
2022-12-06 12:23:43 +0100

    feat(dev): Add Health check command to API
    Related to #23 #19

---
+++ src/backend/thanks-dev/Commands/HealthCheckCommand.cs
@@ -0,0 +1,56 @@
+﻿using Spectre.Console;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+
+namespace thanks_dev.Commands;
+
+internal class HealthCheckCommand : AsyncCommand<HealthCheckSettings>
+{
+    public override async Task<int> ExecuteAsync([NotNull] CommandContext context, [NotNull] HealthCheckSettings settings)
+    {
+        // https://docs.docker.com/engine/reference/builder/#healthcheck
+        // Docker requires either a return code of 0 or 1
+        switch (settings.Type)
+        {
+            case "default":
+            default:
+                return await ExecuteSimpleHealthcheck(settings.APIEndpoint);
+        }
+        
+    }
+
+    private async Task<int> ExecuteSimpleHealthcheck(string? apiEndpoint)
+    {
+        HttpClient http = new HttpClient
+        {
+            Timeout = TimeSpan.FromSeconds(5)
+        };
+
+        try
+        {
+            var response = await http.GetAsync($"{apiEndpoint}/.core/healthcheck");
+            response.EnsureSuccessStatusCode();
+            Console.WriteLine("ok");
+            return 0;
+        }
+        catch (Exception e)
+        {
+            AnsiConsole.WriteException(e);
+            return 1;
+        }
+    }
+}
+
+
+internal class HealthCheckSettings : CommandSettings
+{
+    [CommandArgument(0, "[endpoint]")]
+    [DefaultValue("default")]
+    public string? APIEndpoint { get; set; }
+
+
+    [CommandOption("-t|--type <type>")]
+    [DefaultValue("default")]
+    public string? Type { get; set; }
+
+}
