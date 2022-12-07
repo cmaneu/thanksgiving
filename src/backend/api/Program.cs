b1f2e4eed272db615e16957efed6c84cf79ba250
Christopher MANEU <git@maneu.org>
2022-12-07 11:11:21 +0100

    chore(api): Remove kestrel server header

--- src/backend/api/Program.cs
+++ src/backend/api/Program.cs
@@ -3,6 +3,8 @@ using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options => options.AddServerHeader = false);
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
